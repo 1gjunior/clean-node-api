54b834a9ccb502784f768018c661d118eb8fefe4
Gilberto Junior <junior.siilva182@gmail.com>
2020-11-11 21:49:45 -0300

    move error 401 to a helper method

--- src/presentation/routers/login-router.spec.js
+++ src/presentation/routers/login-router.spec.js
@@ -1,5 +1,6 @@
 const LoginRouter = require('./login-router');
 const MissingParamError = require('../helpers/missing-param-error');
+const UnauthorizedError = require('../helpers/unauthorized-error');
 
 const makeSut = () => {
   class AuthUseCaseSpy {
@@ -78,4 +79,5 @@ test('Should return 401 when invalid credentials are provided', () => {
   };
   const httpResponse = sut.route(httpRequest);
   expect(httpResponse.statusCode).toBe(401);
+  expect(httpResponse.body).toEqual(new UnauthorizedError());
 });
