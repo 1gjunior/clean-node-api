ed3657bdd2a674cb045844758076c2293002eab7
Gilberto Junior <junior.siilva182@gmail.com>
2020-12-01 13:03:44 -0300

    ensure EmailValidator returns false

--- src/utils/email-validator.spec.js
+++ src/utils/email-validator.spec.js
@@ -1,6 +1,8 @@
+const validator = require('validator');
+
 class EmailValidator {
   isValid(email) {
-    return true;
+    return validator.isEmail(email);
   }
 }
 describe('Email Validator', () => {
@@ -8,5 +10,12 @@ describe('Email Validator', () => {
     const sut = new EmailValidator();
     const isEmailValid = sut.isValid('valid_email@mail.com');
     expect(isEmailValid).toBe(true);
+  });
+
+  test('Should return false if validator returns false', () => {
+    validator.isEmailValid = false;
+    const sut = new EmailValidator();
+    const isEmailValid = sut.isValid('invalid_email@mail.com');
+    expect(isEmailValid).toBe(false);
   });
 });
