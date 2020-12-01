5bee895e3d0e54e671eb83ee5fd2aa179049cfc9
Gilberto Junior <junior.siilva182@gmail.com>
2020-12-01 12:49:12 -0300

    ensure EmailValidator returns true

---
+++ src/utils/email-validator.spec.js
@@ -0,0 +1,12 @@
+class EmailValidator {
+  isValid(email) {
+    return true;
+  }
+}
+describe('Email Validator', () => {
+  test('Should return true if validator returns true', () => {
+    const sut = new EmailValidator();
+    const isEmailValid = sut.isValid('valid_email@mail.com');
+    expect(isEmailValid).toBe(true);
+  });
+});
