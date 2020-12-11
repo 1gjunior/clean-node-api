850b6c40022fbe03d511e8b3e342c1837c7aa180
Gilberto Junior <junior.siilva182@gmail.com>
2020-12-11 17:38:18 -0300

    ensure AuthUseCase throws if an invalid LoadUserByEmailRepository is provided

--- src/domain/usecases/auth-usecase.spec.js
+++ src/domain/usecases/auth-usecase.spec.js
@@ -1,4 +1,4 @@
-const { MissingParamError } = require('../../utils/errors');
+const { MissingParamError, InvalidParamError } = require('../../utils/errors');
 
 class AuthUseCase {
   constructor(loadUserByEmailRepository) {
@@ -11,6 +11,12 @@ class AuthUseCase {
     }
     if (!password) {
       throw new MissingParamError('password');
+    }
+    if (!this.loadUserByEmailRepository) {
+      throw new MissingParamError('loadUserByEmailRepository');
+    }
+    if (!this.loadUserByEmailRepository.load) {
+      throw new InvalidParamError('loadUserByEmailRepository');
     }
     await this.loadUserByEmailRepository.load(email);
   }
@@ -44,5 +50,23 @@ describe('Auth UseCase', () => {
     const { sut, loadUserByEmailRepositorySpy } = makeSut();
     await sut.auth('any_email@mail.com', 'any_password');
     expect(loadUserByEmailRepositorySpy.email).toBe('any_email@mail.com');
+  });
+
+  test('Should throw if no LoadUserByEmailRepository is provided', async () => {
+    const sut = new AuthUseCase();
+    const promise = sut.auth('any_email@mail.com', 'any_password');
+    expect(promise).rejects.toThrow(
+      // eslint-disable-next-line comma-dangle
+      new MissingParamError('loadUserByEmailRepository')
+    );
+  });
+
+  test('Should throw if no LoadUserByEmailRepository has no load method', async () => {
+    const sut = new AuthUseCase({});
+    const promise = sut.auth('any_email@mail.com', 'any_password');
+    expect(promise).rejects.toThrow(
+      // eslint-disable-next-line comma-dangle
+      new InvalidParamError('loadUserByEmailRepository')
+    );
   });
 });
